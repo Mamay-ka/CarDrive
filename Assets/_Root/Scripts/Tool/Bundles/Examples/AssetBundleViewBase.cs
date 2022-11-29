9049938ebb1ca9bb920c71160bc82c4000291c35
Mamay-ka <mamay_ka@mail.ru>
2022-11-29 19:40:53 +0300

    ДЗ 9
    Задания от Антона Керпа (автора видеоуроков)
    1. Создание кнопок, их корректная работа и загрузка бандлов

--- Assets/_Root/Scripts/Tool/Bundles/Examples/AssetBundleViewBase.cs
+++ Assets/_Root/Scripts/Tool/Bundles/Examples/AssetBundleViewBase.cs
@@ -8,32 +8,69 @@ namespace Tool.Bundles.Examples
     // комментарии в данном проекте используются только в образовательных целях
     internal class AssetBundleViewBase : MonoBehaviour
     {
+        private const string UrlAssetBundleBackgrounds = "https://drive.google.com/uc?export=download&id=1y2gUPCn4mUzfZ6W8L7K4NeNitS03_Pn0";
         private const string UrlAssetBundleSprites = "https://drive.google.com/uc?export=download&id=1rQzWdcChHhJJBTe4rf1D0Kwi1a43jxWR";
         private const string UrlAssetBundleAudio = "https://drive.google.com/uc?export=download&id=1I7euU6Hv5yrn1ektprUumbGHEikklk3Y";
 
+        [SerializeField] private DataSpriteBundle[] _dataBackgroundBundles;
         [SerializeField] private DataSpriteBundle[] _dataSpriteBundles;
         [SerializeField] private DataAudioBundle[] _dataAudioBundles;
 
+        private AssetBundle _backgroundsAssetBundle;
         private AssetBundle _spritesAssetBundle;
         private AssetBundle _audioAssetBundle;
 
 
-        protected IEnumerator DownloadAndSetAssetBundles()
+        protected IEnumerator DownloadAndSetBackgroundsAssetBundle()
         {
-            // сначала скачиваем нужные бандлы
-            yield return GetSpritesAssetBundle();
-            yield return GetAudioAssetBundle();
+            // скачиваем бандл
+            yield return GetBackgroundsAssetBundle();
 
-            //затем устанавливаем контент из скачанных бандлов
+            // устанавливаем контент из скачанного бандла
+            if (_backgroundsAssetBundle != null)
+                SetBackgroundAssets(_backgroundsAssetBundle);
+            else
+                Debug.LogError($"AssetBundle {nameof(_backgroundsAssetBundle)} failed to load");
+        }
+
+        protected IEnumerator DownloadAndSetSpritesAssetBundles()
+        {
+            // скачиваем бандл
+            yield return GetSpritesAssetBundle();
+
+            // устанавливаем контент из скачанного бандла
             if (_spritesAssetBundle != null)
                 SetSpriteAssets(_spritesAssetBundle);
             else
                 Debug.LogError($"AssetBundle {nameof(_spritesAssetBundle)} failed to load");
+        }
 
+        protected IEnumerator DownloadAndSetAudioAssetBundles()
+        {
+            // скачиваем бандл
+            yield return GetAudioAssetBundle();
+
+            // устанавливаем контент из скачанного бандла
             if (_audioAssetBundle != null)
                 SetAudioAssets(_audioAssetBundle);
             else
                 Debug.LogError($"AssetBundle {nameof(_audioAssetBundle)} failed to load");
+        }
+
+        private IEnumerator GetBackgroundsAssetBundle()
+        {
+            // создаём запрос на получение бандла
+            UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(UrlAssetBundleBackgrounds);
+
+            // отправялем запрос
+            yield return request.SendWebRequest();
+
+            // ждём получение ответа
+            while (!request.isDone)
+                yield return null;
+
+            // получаем из ответа бандл
+            StateRequest(request, out _backgroundsAssetBundle);
         }
 
         private IEnumerator GetSpritesAssetBundle()
@@ -80,6 +117,12 @@ namespace Tool.Bundles.Examples
                 assetBundle = null;
                 Debug.LogError(request.error);
             }
+        }
+
+        private void SetBackgroundAssets(AssetBundle assetBundle)
+        {
+            foreach (DataSpriteBundle data in _dataBackgroundBundles)
+                data.Image.sprite = assetBundle.LoadAsset<Sprite>(data.NameAssetBundle);
         }
 
         private void SetSpriteAssets(AssetBundle assetBundle)
